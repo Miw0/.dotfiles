10f83a73de0c8071eed5e0efc8d3291250df95f3
Miw0 <mw@electronic-minds.de>
2017-11-20 15:02:00 +0100

    :monkey_face:

--- _/wannisfeierabend.sh
+++ _/wannisfeierabend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# via crontab -e: 1 14-18 * * 1-5 $HOME/wannisfeierabend.sh
+# via crontab -e: 1 15-18 * * 1-5 $HOME/wannisfeierabend.sh
 
 environs=`pidof dbus-daemon | tr ' ' '\n' | awk '{printf "/proc/%s/environ ", $1}'`
 export DBUS_SESSION_BUS_ADDRESS=`cat $environs 2>/dev/null | tr '\0' '\n' | grep DBUS_SESSION_BUS_ADDRESS | cut -d '=' -f2-`
